b2c5fc754f806cfa6edc3fb6f2d4a133b8ff76e7
arturo0427 <arturo_munoz27@outlook.com>
2025-10-14 21:49:30 -0500

    Cambios jwt

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -4,9 +4,17 @@ import { env } from "../config/env.js";
 import { loginSchema } from "../interfaces/dtos.js";
 import { validate } from "../middlewares/validate.js";
 import { buildServices } from "./factory.js";
+import * as functions from "firebase-functions";
 
 const router = Router();
 const { users } = buildServices();
+
+const firebaseEnv = functions.config();
+
+const jwtSecret =
+  firebaseEnv?.jwt?.secret ||
+  process.env.JWT_SECRET ||
+  "4c2b8e04e51f7a5c83b4a7c3f17cb8b6b70c4c8dc9f4db84e1ac9b27f2ea56e2";
 
 // Authenticate a user and return a signed JWT token.
 router.post("/login", validate(loginSchema), async (req, res, next) => {
@@ -14,11 +22,11 @@ router.post("/login", validate(loginSchema), async (req, res, next) => {
     const { email, password } = req.body;
     const user = await users.login(email, password);
 
-    if (!env.jwtSecret)
+    if (!jwtSecret)
       throw new Error("Missing JWT secret: Variables" + JSON.stringify(env));
-    const secret: Secret = env.jwtSecret;
+    const secret: Secret = jwtSecret;
     const signOptions: SignOptions = {
-      expiresIn: env.jwtExpiresIn as unknown as number | undefined,
+      expiresIn: env.jwtExpiresIn as unknown as number | "12h",
     };
 
     const token = jwt.sign(

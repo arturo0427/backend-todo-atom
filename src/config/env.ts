00655096e8ded5401a0ebb79445bcb4216004fa4
arturo0427 <arturo_munoz27@outlook.com>
2025-10-14 21:11:59 -0500

    update env

--- src/config/env.ts
+++ src/config/env.ts
@@ -16,35 +16,23 @@ import * as functions from "firebase-functions";
 
 dotenv.config();
 
-const isFirebase = process.env.FIREBASE_CONFIG !== undefined;
-console.log("🚀 ~ isFirebase:", isFirebase);
-
-const getFirebaseConfig = () => {
-  if (!isFirebase) return null;
-
-  try {
-    return functions.config();
-  } catch (error) {
-    console.log("error", error);
-
-    return null;
-  }
-};
-
-const firebaseConfig = getFirebaseConfig();
-console.log("🚀 ~ firebaseConfig:", firebaseConfig);
+const firebaseEnv = functions.config();
 
 export const env = {
   port: Number(process.env.LOCAL_PORT) || 5001,
   nodeEnv: process.env.NODE_ENV || "production",
-  frontendUrl: isFirebase
-    ? firebaseConfig?.env?.frontend_url
-    : process.env.FRONTEND_URL,
-  frontendLocalHostUrl: isFirebase
-    ? firebaseConfig?.env?.frontend_localhost_url
-    : process.env.FRONTEND_LOCALHOST_URL,
-  jwtSecret: isFirebase ? firebaseConfig?.jwt?.secret : process.env.JWT_SECRET,
-  jwtExpiresIn: isFirebase
-    ? firebaseConfig?.jwt?.expires_in
-    : process.env.JWT_EXPIRES_IN,
+  frontendUrl:
+    firebaseEnv?.env?.frontend_url ||
+    process.env.FRONTEND_URL ||
+    "https://todo-1a513.web.app",
+  frontendLocalHostUrl:
+    firebaseEnv?.env?.frontend_localhost_url ||
+    process.env.FRONTEND_LOCALHOST_URL ||
+    "http://localhost:4200",
+  jwtSecret:
+    firebaseEnv?.jwt?.secret ||
+    process.env.JWT_SECRET ||
+    "4c2b8e04e51f7a5c83b4a7c3f17cb8b6b70c4c8dc9f4db84e1ac9b27f2ea56e2",
+  jwtExpiresIn:
+    firebaseEnv?.jwt?.expires_in || process.env.JWT_EXPIRES_IN || "12h",
 };
